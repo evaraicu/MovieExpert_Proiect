06de91dcc5b2fad53548f6176c01ea2a4714739f
evaraicu <evamaria.2003@yahoo.com>
2026-01-15 13:55:39 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,56 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
-using MovieExpert_Proiect.Data;
-using MovieExpert_Proiect.Services;
-using System.Globalization;
-using MovieRecommender_GrpcService;
+using MovieRecommender_GrpcService.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllersWithViews();
-
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-builder.Services.AddHttpClient<MoviePredictionService>();
-
-builder.Services.AddGrpcClient<MovieRecommender_GrpcService.MovieRecommender.MovieRecommenderClient>(o =>
-{
-    o.Address = new Uri("https://localhost:7203");
-});
+builder.Services.AddGrpc();
 
 var app = builder.Build();
 
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
-    try
-    {
-        DbInitializer.Initialize(services);
-    }
-    catch (Exception ex)
-    {
-        var logger = services.GetRequiredService<ILogger<Program>>();
-        logger.LogError(ex, "An error occurred creating the DB.");
-    }
-}
+app.MapGrpcService<MovieRecommenderService>();
 
-var cultureInfo = new CultureInfo("en-US");
-CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
-CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
-
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-app.UseRouting();
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Movies}/{action=Index}/{id?}");
+app.MapGet("/", () => "Service running");
 
 app.Run();
